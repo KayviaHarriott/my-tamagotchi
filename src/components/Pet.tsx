b24c6aa3e9a32fdc871aac06a1df826612f028a2
Kayvia Harriott <kharriott@monainformatixltd.com>
2024-06-01 11:09:04 -0500

    Modified sprite to move in box

--- src/components/Pet.tsx
+++ src/components/Pet.tsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
+// import "./styles/sprite.css";
+import "../styles/sprite.css"
 
 export const PetSprite = () => {
   const [frame, setFrame] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
   const totalFrames = 7; 
   const frameWidth = 64; 
+  const boxSize = 460; 
+//   const [backgroundLocation, setBackgroundPositionLocation] = useState({ x: 0, y: 0 });
+//   const bgSize = { width: 140, height: 70 }; 
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -13,13 +19,47 @@ export const PetSprite = () => {
     return () => clearInterval(interval);
   }, []);
 
+  const handleKeyDown = (e: KeyboardEvent) => {
+    setPosition((prevPosition) => {
+      let { x, y } = prevPosition;
+      switch (e.key) {
+        case 'ArrowUp':
+          y = Math.max(-10, y - 10);
+          break;
+        case 'ArrowDown':
+          y = Math.min(boxSize - 320, y + 10);
+          break;
+        case 'ArrowLeft':
+          x = Math.max(0, x - 10);
+          break;
+        case 'ArrowRight':
+          x = Math.min(boxSize - frameWidth, x + 10);
+          break;
+        default:
+          break;
+      }
+      return { x, y };
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   const backgroundPosition = `-${frame * frameWidth}px 0`;
 
   return (
-    <div>
-      <div className="sprite-container">
-        <div className="sprite" style={{ backgroundPosition }}></div>
-      </div>
+    <div className="sprite-box">
+      <div
+        className="sprite"
+        style={{
+          backgroundPosition,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      ></div>
     </div>
   );
 };
